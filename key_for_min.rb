361749213e0e7417a0efc23cb0adf3c0244601d0
Toron Seigler <toronseigler9@gmail.com>
2019-08-04 16:14:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+
 
 def key_for_min_value(name_hash)
   steak = []
+    adam = [:blake => 500, :ashley => 2, :adam => 1]
 
 end
